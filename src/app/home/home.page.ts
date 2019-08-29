fd6dad4ec4c8b7c36b323662a2821a15617df7b8
Fabio Rolim <fabio_rolim@hotmail.com>
2019-08-29 11:29:41 -0300

    Atualiza home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { core } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nome: string = "Fábio";
+  humor = "sad";
+  checked_humor = false;
+  cores = ["danger", "success", "warning"];
+  idx = 0;
+  cor = "primary";
 
+  constructor() { }
+
+  trocarCor(): void {
+    console.log('Chamou trocarCor!');
+    console.log(this.idx);
+    console.log(this.cores.length);
+    if (this.idx > this.cores.length) {
+      this.idx = 0;
+    }
+    this.cor = this.cores[this.idx];
+    this.idx++;
+  }
+
+  mudaHumor(): void {
+    console.log('Chamou mudaHumor!');
+    this.checked_humor = !this.checked_humor;
+    if (this.checked_humor == true) {
+      this.humor = "happy";
+    } else {
+      this.humor = "sad";
+    }
+  }
 }
